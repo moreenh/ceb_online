487b3d5563674b48368684dc9d7967dfb236db30
yasgun <yasasgunarathne@gmail.com>
2016-12-04 10:42:08 +0530

    added edit_customer_account main to git

---
+++ resources/views/user_employee_type_two/enter_meter_reading/main.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.dashboard')
+
+@section('navbar_items')
+    @include('user_employee_type_two.enter_meter_reading.partials.navbar_items')
+@endsection
+
+@section('body_content')
+    {{--body goes here--}}
+@endsection
